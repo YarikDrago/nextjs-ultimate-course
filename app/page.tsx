d76c37a0df524816ba1cf47ad6b0653f0066f2e3
iu <doudou239@gmail.com>
2025-11-20 20:20:39 +0300

    C5L6: Integrate Inter and Space Grotesk fonts; update layout and main page to utilize new typography.

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div>
+    <>
       <h1 className={"h1-bold"}>Welcome to the Ultimate Next.js course!</h1>
-    </div>
+      <h1 className={"h1-bold font-inter"}>Welcome to the Ultimate Next.js course!</h1>
+      <h1 className={"h1-bold font-space-grotesk"}>Welcome to the Ultimate Next.js course!</h1>
+      <p>Some text</p>
+    </>
   );
 };
 
