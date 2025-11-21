b6ef235074c9edb55e8530e1a071126c10b79e9e
iu <doudou239@gmail.com>
2025-11-21 14:50:02 +0300

    C8L2: Add a dropdown menu component using the ShadCN for example.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 const Page = () => {
   console.log("Page rendered");
   return (
@@ -8,6 +17,17 @@ const Page = () => {
       <h1 className={"h1-bold font-inter"}>Welcome to the Ultimate Next.js course!</h1>
       <h1 className={"h1-bold font-space-grotesk"}>Welcome to the Ultimate Next.js course!</h1>
       <p>Some text</p>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 };
